e7dcc0ad04cdf239169791d2ab6ca729701b3bff
lemoura29 <lemoura29@gmail.com>
2022-07-17 17:22:26 -0400

    Authentication/Authorization in place

--- Model/Appointment.py
+++ Model/Appointment.py
@@ -6,6 +6,21 @@ class Appointment():
         self.start_time = None
         self.end_time = None
 
+
+    def __init__(self, patient_id,  doctor_id, start_time, end_time ):
+        self.patient_id = patient_id
+        self.doctor_id = doctor_id
+        self.start_time = start_time
+        self.end_time = end_time
+class Appointment():
+
+    def __init__(self ):
+        self.patient_id = None
+        self.doctor_id = None
+        self.start_time = None
+        self.end_time = None
+
+
     def __init__(self, patient_id,  doctor_id, start_time, end_time ):
         self.patient_id = patient_id
         self.doctor_id = doctor_id
