befc17a2c878e359b11b7b68d53ce1ab0ca2abf0
lemoura29 <lemoura29@gmail.com>
2022-07-23 18:49:57 -0400

    prescription model

--- Model/Prescription.py
+++ Model/Prescription.py
@@ -1,16 +1,35 @@
-class Prescription():
+from Persistency import Persistency
+import PersistencyDML
 
+
+class Prescription(Persistency):
 
     def __init__(self):
-        self.appointment_id = None
-        self.data_created = None
+        super().__init__()
+        self.patient_id = None
+        self.doctor_id = None
         self.medication = None
-        self.dose = None
         self.observation = None
 
-    def __init__(self, appointment_id,data_created, medication,dose=None, observation=None):
-        self.appointment_id = None
-        self.data_created = None
-        self.medication = None
-        self.dose = None
-        self.observation = None
+    def __init__(self, patient_id, doctor_id, medication, observation=None):
+        super().__init__()
+        self.patient_id = patient_id
+        self.doctor_id = doctor_id
+        self.medication = medication
+        self.observation = observation
+
+    def insertPrescription(self):
+        statement = self.insert_statement_byDict(self.__dict__, 'Prescription')
+        self.execute_command(statement)
+
+
+
+
+
+
+
+
+
+
+
+
