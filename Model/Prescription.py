baca569fac0498fca418f3878c51aef62e47d768
lemoura29 <lemoura29@gmail.com>
2022-07-23 15:55:22 -0400

    fix

--- Model/Prescription.py
+++ Model/Prescription.py
@@ -14,23 +14,3 @@ class Prescription():
         self.medication = None
         self.dose = None
         self.observation = None
-
-        
-class Prescription():
-
-
-    def __init__(self):
-        self.appointment_id = None
-        self.data_created = None
-        self.medication = None
-        self.dose = None
-        self.observation = None
-
-    def __init__(self, appointment_id, data_created, medication,dose=None, observation=None):
-        self.appointment_id = appointment_id
-        self.data_created = data_created
-        self.medication = medication
-        self.dose = dose
-        self.observation = observation
-
-        
